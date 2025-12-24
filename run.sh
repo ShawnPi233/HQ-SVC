4691f80a9100fbc68fcf3c35a1b1402e7884e3c6
shawnpi233 <shawnpi@qq.com>
2025-12-24 08:44:57 +0000

    update debug

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 mkdir -p venv
 tar -xzf environment.tar.gz -C venv
 source venv/bin/activate
-python gradio_app.py
+python gradio_app.py 
+# 如果报错 Caught signal 11 (Segmentation fault: address not mapped to object at address (nil))
+# 请执行 unset LD_LIBRARY_PATH 后再启动代码
